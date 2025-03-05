a6587f3f3e0e27877306d4a0de1c9144c1bc94e0
ariakhademi <ariakhademi@gmail.com>
2025-03-05 10:58:12 -0500

    readme udpdated. tokenize added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nlp
+
+Note: Some of the code in this repo isn't object-oriented because the purpose was just to write the function for practice.
--- tokenizer.py
+++ tokenizer.py
@@ -4,3 +4,14 @@ Create a function to clean and tokenize a given text,
 removing punctuation and converting words to lowercase.
 """
 
+def tokenize(text):
+    punctuations = '''~!@#$%^&*,.()[]\{\}'''
+    for char in text:
+        if char in punctuations:
+            text = text.replace(char,'')
+    text = text.lower()
+    text = text.split()
+    return text
+
+text = 'I am writing the code.'
+print(tokenize(text=text))
