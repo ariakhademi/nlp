b73fc0a610bab502e1d4f84f35b5191e670147c7
ariakhademi <ariakhademi@gmail.com>
2025-03-05 11:42:05 -0500

    stop word removal

--- stop_word_removal.py
+++ stop_word_removal.py
@@ -4,3 +4,21 @@ Develop a function that removes stopwords
 (commonly used words) from a given sentence or text. 
 """
 
+def stop_word_removal(text):
+    # expand as needed
+    stop_words = {
+        'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 
+        'has', 'he', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 
+        'to', 'was', 'were', 'will', 'with', 'i', 'you', 'your', 'am'
+    }
+
+    text = text.lower().split()
+    text = [word for word in text if word not in stop_words]
+    text = ' '.join(text)
+
+    return text
+
+text = 'I am a computer scientist who practices coding.'
+print(stop_word_removal(text=text))
+
+
