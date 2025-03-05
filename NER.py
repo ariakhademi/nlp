8dd8dea4f2ceb7a6f1661c88df1b0c90a003fc46
ariakhademi <ariakhademi@gmail.com>
2025-03-05 14:11:56 -0500

    NER

--- NER.py
+++ NER.py
@@ -4,3 +4,13 @@ Implement a Named Entity Recognition algorithm
 that identifies and classifies named entities 
 like names, locations, and organizations in a text. 
 """
+from spacy import nlp
+
+def my_ner(text):
+    doc = nlp(text)
+    entities = [(entity.text , entity.label_) for entity in doc.entities]
+
+    return entities
+
+text = 'I have worked at Ford and IEEE from home in Pennsylvania.'
+print(my_ner(text=text))
