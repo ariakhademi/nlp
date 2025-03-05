e65d3db40119bf91db25ad82ad02679a6b342cd9
ariakhademi <ariakhademi@gmail.com>
2025-03-05 11:51:36 -0500

    NER initiated

---
+++ NER.py
@@ -0,0 +1,6 @@
+"""
+Named Entity Recognition (NER) 🧐 Challenge: 
+Implement a Named Entity Recognition algorithm 
+that identifies and classifies named entities 
+like names, locations, and organizations in a text. 
+"""
